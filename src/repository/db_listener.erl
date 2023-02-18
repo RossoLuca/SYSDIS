a8267eaaf0da3eb9c7a52f968d6a799a5320831d
Andrea Zarola <andreazarola@gmail.com>
2023-02-18 11:42:32 +0100

    Added 'fallen' field in delivery definition

--- src/repository/db_listener.erl
+++ src/repository/db_listener.erl
@@ -11,37 +11,55 @@ start_link(_Name) ->
 loop() -> 
     receive
         {write, {_FromNode, FromPid}, Table, Data} ->
-            % To replace with a log util
-            io:format("Received write action from ~p on table ~p of ~p~n", [FromPid, Table, Data]),
-            % To be added check that Table is defined on db and that Data match Table's attriutes
-            WriteFun = fun() -> mnesia:write(Data) end,
-            {Status, Result} = execute(WriteFun),
-            FromPid ! {result, Status, Result},
-            loop();
-        {select, {_FromNode, FromPid}, Table, Specification} -> 
-            io:format("Received select action from ~p on table ~p of ~p~n", [FromPid, Table, Specification]),
-            % To be added check that Table is defined on db
-            SelectFun = fun() -> mnesia:select(Table, Specification) end,
-            {Status, Result} = execute(SelectFun),
-            io:format("~n ~p ~n ~p", [Status, Result]),
-            if Status == aborted ->
-                    FromPid ! {result, Status, Result};
+            case check_table_exists(Table) of
                 true ->
-                    List = lists:map(fun(T) -> lists:nth(1, T) end, Result),
-                    AdaptedResult = adaptResponse(List),
-                    FromPid ! {result, Status, AdaptedResult}
+                    io:format("Received write action from ~p on table ~p of ~p~n", [FromPid, Table, Data]),
+                    % To be added check that Data match Table's attributes
+                    WriteFun = fun() -> mnesia:write(Data) end,
+                    {Status, Result} = execute(WriteFun),
+                    FromPid ! {result, Status, Result};
+                false ->
+                    io:format("Received write action from ~p on table ~p that is not defined~n", [FromPid, Table]),
+                    {Status, Reason} = {aborted, table_not_exists},
+                    FromPid ! {result, Status, Reason}
             end,
             loop();
+        {select, {_FromNode, FromPid}, Table, Specification} ->
+            case check_table_exists(Table) of
+                true -> 
+                    io:format("Received select action from ~p on table ~p of ~p~n", [FromPid, Table, Specification]),
+                    SelectFun = fun() -> mnesia:select(Table, Specification) end,
+                    {Status, Result} = execute(SelectFun),
+                    io:format("~n ~p ~n ~p", [Status, Result]),
+                    if Status == aborted ->
+                            FromPid ! {result, Status, Result};
+                        true ->
+                            List = lists:map(fun(T) -> lists:nth(1, T) end, Result),
+                            AdaptedResult = adaptResponse(List),
+                            FromPid ! {result, Status, AdaptedResult}
+                    end;
+                false ->
+                    io:format("Received select action from ~p on table ~p that is not defined~n", [FromPid, Table]),
+                    {Status, Reason} = {aborted, table_not_exists},
+                    FromPid ! {result, Status, Reason}
+                end,
+            loop();
         {read_by_id, {_FromNode, FromPid}, Table, Id} ->
-            io:format("Received read by id action from ~p on table ~p of ~p~n", [FromPid, Table, Id]),
-            % To be added check that Table is defined on db
-            ReadFun = fun() -> mnesia:read({Table, Id}) end,
-            {Status, Result} = execute(ReadFun),
-            if Status == aborted ->
-                    FromPid ! {result, Status, Result};
+            case check_table_exists(Table) of
                 true ->
-                    AdaptedResult = adaptResponse(Result),
-                    FromPid ! {result, Status, AdaptedResult}
+                    io:format("Received read by id action from ~p on table ~p of ~p~n", [FromPid, Table, Id]),
+                    ReadFun = fun() -> mnesia:read({Table, Id}) end,
+                    {Status, Result} = execute(ReadFun),
+                    if Status == aborted ->
+                            FromPid ! {result, Status, Result};
+                        true ->
+                            AdaptedResult = adaptResponse(Result),
+                            FromPid ! {result, Status, AdaptedResult}
+                    end;
+                false ->
+                    io:format("Received read by id action from ~p on table ~p that is not defined~n", [FromPid, Table]),
+                    {Status, Reason} = {aborted, table_not_exists},
+                    FromPid ! {result, Status, Reason}
             end,
             loop();
         {_Operation, {_FromNode, FromPid}, _Table, _Specification} -> 
@@ -61,12 +79,27 @@ adaptResponse(Deliveries) ->
     lists:map(fun(Del) -> 
                 #{id => Del#delivery.id,
                     pid => Del#delivery.pid,
-                    stato => Del#delivery.stato,
+                    state => Del#delivery.state,
                     start_x => Del#delivery.start_x,
                     start_y => Del#delivery.start_y,
                     current_x => Del#delivery.current_x,
                     current_y => Del#delivery.current_y,
                     end_x => Del#delivery.end_x,
-                    end_y => Del#delivery.end_y
+                    end_y => Del#delivery.end_y,
+                    fallen => Del#delivery.fallen
                 }  
     end, Deliveries).
+
+
+check_table_exists(Table) ->
+    Tables = mnesia:system_info(tables),
+    case lists:search(fun(T) ->
+                        if Table == T ->
+                            true;
+                        true ->
+                            false
+                        end
+                end, Tables) of
+        {value, Table} -> true;
+        false -> false
+    end.
